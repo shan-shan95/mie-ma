abf5c27473f643b3bbf18ca5d1319b4549124151
shang-shang95 <qqxn7ra9k@gmail.com>
2019-01-11 04:09:08 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 6
-  },
-  extends: [
-    'plugin:vue/essential',
-    "eslint:recommended",
-    "plugin:prettier/recommended"
-  ],
-  plugins: [
-    'vue'
-  ],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        "singleQuote": true,
-        "semi": false,
-        "files.associations": {
-          "*.vue": "vue"
-        },
-        "emmet.syntaxProfiles": {
-          "vue": "css"
-        },
-        "indent": ["error", "tab"],
-      },
-    ],
-  }
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/recommended",
+		"plugin:prettier/recommended",
+	],
+	plugins: [
+		'vue'
+	],
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+				"singleQuote": true,
+				"semi": false,
+				"indent": ["error", 2],
+			},
+		],
+	}
 }
