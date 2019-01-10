55065ff354744e6af4a245ff942a4f99820b1359
shang-shang95 <qqxn7ra9k@gmail.com>
2019-01-11 04:17:39 +0900

    Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = (env, argv) => {
+  const IS_DEV = argv.mode === 'development'
+
+  return {
+    entry: ['babel-polyfill', './app/assets/javascripts/application.js'],
+    output: {
+      filename: 'main.js',
+      path: `${__dirname}/dist`
+    },
+    devtool: IS_DEV ? 'source-map' : 'none',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              js: 'babel-loader!eslint-loader',
+              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            }
+          }
+        }
+      ]
+    }
+  }
+}
