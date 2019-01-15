3453af9cb5d982eca67651e6ab0feeb0c74048a2
shang-shang95 <qqxn7ra9k@gmail.com>
2019-01-15 10:31:57 +0900

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,89 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+
 module.exports = (env, argv) => {
   const IS_DEV = argv.mode === 'development'
 
   return {
-    entry: ['babel-polyfill', './app/assets/javascripts/application.js'],
-    output: {
-      filename: 'main.js',
-      path: `${__dirname}/public/packs`
+    entry: {
+      main: './frontend/application.js'
     },
     devtool: IS_DEV ? 'source-map' : 'none',
+    output: {
+      filename: 'javascripts/[name]-[hash].js',
+      path: path.resolve(__dirname, 'public', 'assets')
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'stylesheets/[name]-[hash].css'
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new ManifestPlugin({
+        writeToFileEmit: true
+      })
+    ],
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         },
         {
           test: /\.vue$/,
-          loader: 'vue-loader',
-          options: {
-            loaders: {
-              js: 'babel-loader!eslint-loader',
-              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.pug/,
+          loader: 'pug-plain-loader'
+        },
+        {
+          test: /\.(c|sc)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '/public/assets/stylesheets/'
+              }
             },
-            extractCSS: true
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(jpg|png|gif)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name]-[hash].[ext]',
+            outputPath: 'images/',
+            publicPath: function(path) {
+              return '../images/' + path
+            }
           }
         }
       ]
+    },
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.js'
+      },
+      extensions: ['.js', '.scss', 'css', '.vue', '.jpg', '.png', '.gif', ' ']
+    },
+    devServer: {
+      host: 'localhost',
+      port: 3035,
+      publicPath: 'http://localhost:3035/public/assets/',
+      contentBase: path.resolve(__dirname, 'public', 'assets'),
+      hot: true,
+      disableHostCheck: true,
+      historyApiFallback: true
     }
   }
 }
