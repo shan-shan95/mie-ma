b7dfe48e03418f34c91b411b001bd3a9695a8f01
shang-shang95 <qqxn7ra9k@gmail.com>
2019-01-26 09:09:49 +0900

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,21 +15,27 @@ module.exports = (env, argv) => {
   const IS_DEV = argv.mode === 'development'
 
   return {
-    entry: entries,
+    entry: {
+      application: [
+        './frontend/init/application.js',
+        './frontend/init/application.scss'
+      ],
+      ...entries
+    },
     devtool: IS_DEV ? 'source-map' : 'none',
     output: {
       filename: 'javascripts/[name]-[hash].js',
-      path: path.resolve(__dirname, 'public/assets')
+      path: path.resolve(__dirname, 'public/assets/')
     },
     plugins: [
       new VueLoaderPlugin(),
       new MiniCssExtractPlugin({
         filename: 'stylesheets/[name]-[hash].css'
       }),
-      new webpack.HotModuleReplacementPlugin(),
       new ManifestPlugin({
         writeToFileEmit: true
-      })
+      }),
+      new webpack.HotModuleReplacementPlugin()
     ],
     module: {
       rules: [
@@ -65,7 +71,10 @@ module.exports = (env, argv) => {
             {
               loader: MiniCssExtractPlugin.loader,
               options: {
-                publicPath: path.resolve(__dirname, 'public/assets/stylesheets')
+                publicPath: path.resolve(
+                  __dirname,
+                  'public/assets/stylesheets/'
+                )
               }
             },
             'css-loader',
@@ -77,11 +86,23 @@ module.exports = (env, argv) => {
           loader: 'file-loader',
           options: {
             name: '[name]-[hash].[ext]',
-            outputPath: 'images',
+            outputPath: 'images/',
             publicPath: function(path) {
               return 'images/' + path
             }
           }
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'fonts/[name].[ext]',
+                publicPath: '../'
+              }
+            }
+          ]
         }
       ]
     },
@@ -89,7 +110,21 @@ module.exports = (env, argv) => {
       alias: {
         vue: 'vue/dist/vue.js'
       },
-      extensions: ['.js', '.scss', 'css', '.vue', '.jpg', '.png', '.gif', ' ']
+      extensions: [
+        '.js',
+        '.scss',
+        'css',
+        '.vue',
+        '.jpg',
+        '.png',
+        '.gif',
+        '.woff',
+        '.woff2',
+        '.svg',
+        '.ttf',
+        '.eot',
+        ' '
+      ]
     },
     optimization: {
       splitChunks: {
@@ -101,16 +136,21 @@ module.exports = (env, argv) => {
             enforce: true
           }
         }
-      }
+      },
+      minimize: true
     },
     devServer: {
       host: 'localhost',
       port: 3035,
       publicPath: 'http://localhost:3035/public/assets/',
-      contentBase: path.resolve(__dirname, 'public/assets'),
+      contentBase: path.resolve(__dirname, 'public/assets/'),
       hot: true,
       disableHostCheck: true,
-      historyApiFallback: true
+      historyApiFallback: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
     }
   }
 }
