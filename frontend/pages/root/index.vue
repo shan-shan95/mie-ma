48f13b6eef5dcaa1b88981bb22338e1d5d986913
shang-shang95 <qqxn7ra9k@gmail.com>
2019-01-26 09:08:12 +0900

    Update view file

--- frontend/pages/root/index.vue
+++ frontend/pages/root/index.vue
@@ -3,6 +3,10 @@
   App
   p Index page
   img(src='../../images/image.jpg')
+  a.button.is-primary.fas.fa-igloo Button
+  i.fas.fa-igloo Item
+  span.fas.fa-ad
+  p.fas.fa-ad こんにちは
 </template>
 
 <style lang="scss"></style>
