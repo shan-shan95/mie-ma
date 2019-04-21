d809ab6bdb82a69653f495ecdee36fb1dad02ab0
shang-shang95 <qqxn7ra9k@gmail.com>
2019-04-22 04:06:27 +0900

    Update item model

---
+++ app/models/concerns/id_generator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module IdGenerator
+  def self.included(klass)
+    klass.before_create :fill_id
+  end
+
+  def fill_id
+    self.id = loop do
+      uuid = SecureRandom.uuid
+      break uuid unless self.class.exists?(id: uuid)
+    end
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
+  include IdGenerator
+
+  belongs_to :buyer, class_name: 'User'
+  belongs_to :seller, class_name: 'User'
+
+  before_create :set_start_on
+
+  enum status: {
+    :brand_new, #新品
+    :excellent, #良品
+    :poor, #傷あり
+    :junk #ジャンク
+  }
+
+  private
+
+  def set_start_on
+    start_on = Date.today
+  end
 end
