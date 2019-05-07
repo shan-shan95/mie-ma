e73170f5d644eab831576f7839aac0cee4fa0aaf
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-07 18:40:53 +0900

    Create ItemForm component

---
+++ frontend/components/ItemForm.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+.form
+  form(
+    action="/items"
+    accept-charset="UTF-8"
+    method="post"
+  )
+    input(
+      name="utf8"
+      type="hidden"
+      value="✓"
+    )
+    input(
+      name="authenticity_token"
+      type="hidden"
+      value="csrf_token"
+    )
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label(for="item_name") 名称
+      .field-body
+        input.input#item_name(
+          autofocus="autofocus"
+          autocomplete="off"
+          type="text"
+          name="item[name]"
+          v-model="item.name"
+        )
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label(for="item_description") 説明
+      .field-body
+        .field
+          .control
+            textarea.textarea#item_description(
+              name="item[description]"
+              autocomplete="off"
+              v-model="item.description"
+            )
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label(for="item_status") 状態
+      .field-body
+        .field.is-narrow
+          .control
+            .select
+              select#item_status(
+                name="item[status]"
+                v-model.number="item.status"
+              )
+                option(value="null" disabled) 選択して下さい
+                option(value="0") 新品
+                option(value="1") 良品
+                option(value="2") 傷あり
+                option(value="3") ジャンク品
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label(for="item_price") 値段
+      .field-body
+        .field.is-narrow
+          .control
+            input.input(
+              autocomplete="off"
+              type="number"
+              name="item[price]"
+              v-model="item.price"
+            )
+    input(
+      type="hidden",
+      name="item[seller_id]"
+      v-model="item.seller_id"
+    )
+    .field.is-horizontal
+      .field-label
+      .field-body
+        .field
+          .control
+            button.button.is-info.submit(
+              type="submit"
+            ) 送信
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      item: this.newItem
+    }
+  },
+  props: {
+    newItem: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  margin: 2rem 5rem;
+}
+</style>
--- frontend/pages/items/new.vue
+++ frontend/pages/items/new.vue
@@ -1,74 +1,7 @@
 <template lang="pug">
 #new
   Header
-  main
-    form(
-      action="/items"
-      accept-charset="UTF-8"
-      method="post"
-    )
-      input(
-        name="utf8"
-        type="hidden"
-        value="✓"
-      )
-      input(
-        name="authenticity_token"
-        type="hidden"
-        value="csrf_token"
-      )
-      .field
-        label(for="item_name") 名称
-        br
-        input#item_name(
-          autofocus="autofocus"
-          autocomplete="off"
-          type="text"
-          name="item[name]"
-          v-model="item.name"
-        )
-      .field
-        label(for="item_description") 説明
-        br
-        textarea#item_description(
-          name="item[description]"
-          autocomplete="off"
-          cols="24"
-          rows="12"
-          v-model="item.description"
-        )
-      .field
-        label(for="item_status") 状態
-        br
-        select#item_status(
-          name="item[status]"
-          v-model.number="item.status"
-        )
-          option(value="null" disabled) 選択して下さい
-          option(value="0") 新品
-          option(value="1") 良品
-          option(value="2") 傷あり
-          option(value="3") ジャンク品
-      .field
-        label(for="item_price") 値段
-        br
-        input(
-          autocomplete="off"
-          type="number"
-          name="item[price]"
-          v-model="item.price"
-        )
-      input(
-        type="hidden",
-        name="item[seller_id]"
-        v-model="item.seller_id"
-      )
-      .submit
-        button(
-          type="submit"
-          data-disable-with="送信中..."
-          @click="onSubmit()"
-        ) 投稿
+  Form(:newItem="newItem")
   Footer
 </template>
 
@@ -76,25 +9,19 @@
 </style>
 
 <script>
-import axios from 'axios'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+import Form from '../../components/ItemForm'
 
 export default {
   components: {
     Header,
-    Footer
+    Footer,
+    Form
   },
   data() {
     return {
-      item: gon.item
-    }
-  },
-  methods: {
-    onSubmit() {
-      axios.post('/items', {
-        params: this.item
-      })
+      newItem: gon.item
     }
   }
 }
