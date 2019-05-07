58c7a531fb8b1f924b9e774a40c026966d6eaf52
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-07 18:42:20 +0900

    Add new validation to eval_comment

--- app/models/evaluation_comment.rb
+++ app/models/evaluation_comment.rb
@@ -1,7 +1,8 @@
 class EvaluationComment < ApplicationRecord
   belongs_to :user
 
-  validates :comment, length: {maximum: 255}
+  validates :status, presence: true
+  validates :comment, length: {maximum: 255}, presence: true
 
   enum status: %i[good normal bad]
 end
