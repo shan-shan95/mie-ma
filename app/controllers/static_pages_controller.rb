9e12e6890d99baecd7fe98963e17741e4417f297
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-08 22:55:50 +0900

    Create static page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def sell_guide
+  end
+
+  def buy_guide
+  end
+
+  def contact
+  end
+
+  def privacy_policy
+  end
+
+  def terms_of_service
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,11 @@ Rails.application.routes.draw do
 
   resources :evaluation_comments, only: :create
 
+  get "sell_guide", to: "static_pages#sell_guide" # 出品ガイド
+  get "buy_guide", to: "static_pages#buy_guide" # 購入ガイド
+  get "contact", to: "static_pages#contact" # 問い合わせ
+  get "privacy_policy", to: "static_pages#privacy_policy" # プライバシーポリシー
+  get "terms_of_service", to: "static_pages#terms_of_service"  # 利用規約
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
