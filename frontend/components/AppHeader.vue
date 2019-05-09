25c2571c0106bd0177e98c60dae33487dddecee0
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-09 16:05:04 +0900

    Update logo

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -2,7 +2,7 @@
 nav.navbar(role='navigation' aria-label='main navigation')
   .navbar-brand
     a.navbar-item.logo(href='/')
-      img(src='../images/logo.png' alt='top-banner' width='240' height='192')
+      img.logo(src='../images/logo.png' alt='top-banner')
     a.navbar-burger.burger(role='button', aria-label='menu', aria-expanded='false' data-target='navbarBasicExample', @click="toggleIsActive()", :class="{ 'is-active': this.isActive, 'top': !this.isActive }")
       span(aria-hidden='true')
       span(aria-hidden='true')
@@ -42,9 +42,13 @@ export default {
 <style lang="scss" scoped>
 .navbar {
   box-shadow: 2px 2px 4px gray;
-}
-.navbar-item img {
-  max-height: 10rem;
+
+  .logo {
+    margin-left: 2em;
+    max-height: 3rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
 }
 .navbar-burger {
   height: 5rem;
@@ -69,9 +73,6 @@ export default {
       top: calc(50% + 7px);
     }
   }
-}
-.logo {
-  padding-left: 2rem;
 }
 .button-item {
   padding-right: 3rem;
