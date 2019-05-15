03071cb50659c95316514d70fe290b872fbabda6
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-16 01:17:03 +0900

    Change method to public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :signed_in_gon
 
-  protected
-
-  def signed_in_gon
-    gon.is_signed_in = user_signed_in?
-  end
-
   def render(*args, &block)
     gon.notice = flash[:notice]
     gon.alert = flash[:alert]
     super
+  end
+
+  protected
+
+  def signed_in_gon
+    gon.is_signed_in = user_signed_in?
   end
 end
