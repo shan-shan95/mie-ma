d2f138b1c8aae067e50a1dc944364bec139421ee
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-17 03:37:58 +0900

    Set validation callback

--- app/controllers/evaluation_comments_controller.rb
+++ app/controllers/evaluation_comments_controller.rb
@@ -3,10 +3,8 @@ class EvaluationCommentsController < ApplicationController
 
   def create
     item = Item.find(params[:item_id])
-    comment = EvaluationComment.new(evaluation_comment_params)
-    if comment.save
+    if EvaluationComment.create(evaluation_comment_params)
       item.update(trading_status: :completed)
-      update_evaluation(params[:evaluation_comment][:seller_id], params[:evaluation_comment][:status])
       return redirect_to item_path(item), notice: "評価が完了しました"
     else
       return redirect_to trading_item_path(item), alert: "評価の投稿に失敗しました。"
@@ -17,11 +15,5 @@ class EvaluationCommentsController < ApplicationController
 
   def evaluation_comment_params
     params.require(:evaluation_comment).permit(:seller_id, :buyer_id, :status, :comment)
-  end
-
-  def update_evaluation(user_id, status)
-    UserEvaluation.create(user_id: user_id) unless User.find(user_id).evaluation.present?
-    eval = User.find(user_id).evaluation
-    eval.update(status.to_sym => eval[status] + 1)
   end
 end
--- app/models/evaluation_comment.rb
+++ app/models/evaluation_comment.rb
@@ -2,8 +2,18 @@ class EvaluationComment < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
 
+  before_create :create_user_eval
+
   validates :status, presence: true
   validates :comment, length: {maximum: 255}
 
   enum status: %i[good normal bad]
+
+  private
+
+  def create_user_eval
+    eval = User.find(seller_id).evaluation
+    eval = UserEvaluation.create(user_id: seller_id) if eval.nil?
+    eval.update(status.to_sym => eval[status] + 1)
+  end
 end
