b4994e983b05ea25c79bd19e88c97c3f19b9b4c3
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-28 21:11:10 +0900

    Display number of evaluated comment

--- frontend/pages/mypages/EvaluationComments.vue
+++ frontend/pages/mypages/EvaluationComments.vue
@@ -16,30 +16,40 @@
                   :class="{'selected': isSelectedEval('good')}"
                   @click="selectEval('good')"
                 )
-                  i.far.fa-laugh.icon.is-medium.fa-lg.i-center
-                  span.is-size-5 Good
+                  template
+                    i.far.fa-laugh.icon.is-medium.fa-lg.i-center
+                    span.is-size-5 良い
+                  template
+                    p {{ selectedEvalComments('good').length }}
                 a.evaluation.normal.has-text-centered(
                   :class="{'selected': isSelectedEval('normal')}"
                   @click="selectEval('normal')"
                 )
-                  i.far.fa-meh.icon.is-medium.fa-lg.i-center
-                  span.is-size-5 Normal
+                  template
+                    i.far.fa-meh.icon.is-medium.fa-lg.i-center
+                    span.is-size-5 普通
+                  template
+                    p {{ selectedEvalComments('good').length }}
                 a.evaluation.bad.has-text-centered(
                   :class="{'selected': isSelectedEval('bad')}"
                   @click="selectEval('bad')"
                 )
-                  i.far.fa-frown.icon.is-medium.fa-lg
-                  span.is-size-5 Bad
+                  template
+                    i.far.fa-frown.icon.is-medium.fa-lg
+                    span.is-size-5 イマイチ
+                  template
+                    p {{ selectedEvalComments('bad').length }}
               hr.hr
-              .message-background
-                .message-column(
+              .comments-background
+                .comment-column(
                   v-for="evaluation in selectedEvalComments(selectedEval)"
                   :key="evaluation.id"
                 )
                   .user-info
                     p.user-name {{ evaluation.buyer_name }}
-                  .message
-                    p.message-content {{ evaluation.comment }}
+                    i.far.fa-laugh.i-center
+                  .comment
+                    p.comment-content {{ displayComment(evaluation.comment) }}
               p(v-if="selectedEvalComments(selectedEval).length === 0") この項目には表示できるコメントがありません
   Footer
 </template>
@@ -69,9 +79,14 @@ export default {
       this.selectedEval = evaluation
     },
     selectedEvalComments(evaluation) {
-      return this.evalComments.filter(
-        x => x.status === evaluation && x.comment !== ''
-      )
+      return this.evalComments.filter(x => x.status === evaluation)
+    },
+    displayComment(comment) {
+      if (comment === '') {
+        return ''
+      } else {
+        return comment
+      }
     }
   }
 }
@@ -110,6 +125,27 @@ export default {
     margin: 0.5rem auto;
     margin-bottom: 1rem;
     background-color: #aaaaaa;
+  }
+
+  .comments-background {
+    background-color: rgba(120, 255, 200, 0.3);
+    width: 100%;
+    border-radius: 5px;
+    padding: 2rem;
+
+    .user-info {
+      display: flex;
+    }
+
+    .comment {
+      margin-left: 1rem;
+      padding: 12px 8px;
+      background-color: lightgrey;
+      display: inline-block;
+      border-radius: 10px;
+      border: 1px solid #9c9c9c;
+      word-wrap: break-word;
+    }
   }
 }
 </style>
