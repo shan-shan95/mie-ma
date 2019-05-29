b786d767df6225a148aff1500ea65f5d77eb947d
shang-shang95 <qqxn7ra9k@gmail.com>
2019-05-30 07:06:44 +0900

    Set current user gon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :signed_in_gon
+  before_action :signed_in_gon, :current_user_gon
 
   def render(*args, &block)
     gon.notice = flash[:notice]
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def signed_in_gon
     gon.is_signed_in = user_signed_in?
+  end
+
+  def current_user_gon
+    gon.current_user = current_user.slice(:id, :nickname) if user_signed_in?
   end
 end
