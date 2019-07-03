aa4670589ca1c752687c25be4edf19c9070dc593
shang-shang95 <qqxn7ra9k@gmail.com>
2019-07-04 06:12:24 +0900

    tweak

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -6,14 +6,14 @@ class MypagesController < ApplicationController
   end
 
   def evaluation_comments
-    gon.eval_comments = current_user.evaluation_comments.map(&:evaluator_name)
+    gon.eval_comments = current_user.evaluation_comments&.map(&:evaluator_name)
   end
 
   def sell_items
-    gon.sell_items = current_user.sell_items.map(&:with_sumbnail_url)
+    gon.sell_items = current_user.sell_items&.map(&:with_sumbnail_url)
   end
 
   def buy_items
-    gon.buy_items = current_user.buy_items.map(&:with_sumbnail_url)
+    gon.buy_items = current_user.buy_items&.map(&:with_sumbnail_url)
   end
 end
